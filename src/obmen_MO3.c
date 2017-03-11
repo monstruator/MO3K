de1d998d90630b90f73274c7a23ca34cca565bd9
monstruator <ya.paval@ya.ru>
2017-03-11 11:58:03 +0300

    доработан обмен и установка реле. в прибор НЕ записано

--- src/obmen_MO3.c
+++ src/obmen_MO3.c
@@ -33,23 +33,17 @@
 	float Mc=64.*1852./32384./3600., Dbl, Flt=0;
 	int sock, length, i , count_mes=0;
 	static Udp_Client_t Uc41,Uc42;
-	char bufi[1024];
-	char bufo[1024];
-	char gloria_start=0;	
-	char gloria_count=0;	
+	char bufi[1024],bufo[1024],but_buf[1024],gloria_start=0,gloria_count=0;
+	obmen_MO3_MO3K_t rec4;	
 	unsigned int pr1_c_old=0;
 	unsigned int AK_c=0; //счетчик обмена после поступления команды АК
 	short bM4; //буфер для M4
 	short cr_com41=0,cr_com42=0,cr_comAK=0;
-	char out_buf[1024];
-	//obmen_41_31_t from41;	
-	//obmen_31_41_t to41;	
 	paramAKcom=0;
 	simfonia41_t simfonia;	
 	int r,bytes,byta4;
 	pid_t pid_CEP;
 	short MK2[15];
-	//unsigned short buf;
 	short byta2,T,len_OUT,sen,j;
     div_t   vol;    // vol.quot - количество полных томов
     char          pack_buf[1500];  // буфер задачи obm_41_31. Выходные данные в Socket
@@ -100,20 +94,22 @@ while(1)
 //	printf(" read=%d size1=%d size2=%d size3=%d sizeALL=%d\n",
 //	bytes,sizeof(obmen_42_31_2t),sizeof(obmen_41_31_2t),sizeof(obmen_AK_MN3_MO3K_t),sizeof(obmen_MO3_MO3K_t));
 
-    memcpy(&p->from_MO3,&bufi[4],sizeof(obmen_MO3_MO3K_t)); 
+    //memcpy(&p->from_MO3,&bufi[4],sizeof(obmen_MO3_MO3K_t)); 
+	memcpy(&rec4,&bufi[4],sizeof(obmen_MO3_MO3K_t)); 
 	//выбор управляюще1 команды
-	if (p->from_MO3.from42.cr_com!=cr_com42) 
+	if (rec4.from42.cr_com!=cr_com42) 
 	{
 		printf(" New Command 4.2 = %d  cr_com = %d\n",
 			p->from_MO3.from42.num_com,p->from_MO3.from42.cr_com);
 
-		p->num_com=p->from_MO3.from42.num_com;
-		cr_com42=p->from_MO3.from42.cr_com;
+		p->num_com=rec4.from42.num_com;
+		cr_com42=rec4.from42.cr_com;
+		p->from_MO3.from42=rec4.from42;
 		if (p->num_com==5)
 		{
 			p->M[0]=p->from_MO3.from42.M1;
 			p->M[1]=p->from_MO3.from42.M2;
-			p->M[2]=p->from_MO3.from42.M3;
+			p->M[2]=p->from_MO3.from42.M3&0xFFFE;
 			p->M[3]=p->from_MO3.from42.M4;
 			buf[0]=0;	//переворачиваем управляющие слова пр. 1
 			for(i1=0;i1<16;i1++) {buf[0]+=((p->M[0]>>i1)&1)<<(15-i1);} p->M[0]=buf[0];buf[0]=0;
@@ -122,61 +118,93 @@ while(1)
 			for(i1=0;i1<16;i1++) {buf[0]+=((p->M[3]>>i1)&1)<<(15-i1);} p->M[3]=buf[0];buf[0]=0;
 		}
 	}
-	if ((p->from_MO3.fromAK.cr_com!=cr_comAK)&&(p->from_MO3.fromAK.num_com!=0)) 
+	if ((rec4.fromAK.cr_com!=cr_comAK)&&(rec4.fromAK.num_com!=0)) 
 	{
-		p->num_com=p->from_MO3.fromAK.num_com;
-		cr_comAK=p->from_MO3.fromAK.cr_com;
+		p->num_com=rec4.fromAK.num_com;
+		cr_comAK=rec4.fromAK.cr_com;
+		p->from_MO3.fromAK=rec4.fromAK;
 		pr1_c_old=p->pr1_c;	//сохраним счетчик обмена с пр.1
 		AK_c=1;
 		p->to_MO3.toAK.kzv=0;
 		printf(" New Command AK = %d , p[0]=%d , cr_com = %d\n",
 				p->from_MO3.fromAK.num_com,p->from_MO3.fromAK.a_params[0],p->from_MO3.fromAK.cr_com);
-		p->M[2]=0x0001; //РЭЛЕ АК
+		if ((p->num_com==301)&&(p->num_com==300)) p->M[2]=p->M[2]|0x0001; //РЭЛЕ АК
 		bM4=0;
 		paramAKcom=0;
 		switch(p->num_com)
 		{
-			case 260 : case 290 :	paramAKcom=1;break;//8410
-			case 264 :				paramAKcom=2;break;
-			case 265 : 	switch(p->from_MO3.fromAK.a_params[0])
-						{
-							case 2:	case 3: paramAKcom=3;break; 
-							case 4: case 5: 
-							case 6:	case 7: 
-							case 8: case 9: paramAKcom=4;break;
-						}
-						break;
-			case 292 : 	switch(p->from_MO3.fromAK.a_params[0])
-						{
-							case 1: paramAKcom=1;break;
-							case 3:	paramAKcom=2;break;//C430
-							case 5: paramAKcom=3;break;//9450
-						}
-						break;
 			case 300 : 	switch(p->from_MO3.fromAK.a_params[0])
 						{
-							case 0: paramAKcom=1;break;
-							case 1:	paramAKcom=3;break;
+							case 0: 
+							 
+							     {paramAKcom=1;p->M[2]=p->M[2]|0x0001;printf("PRM\n");} 
+							//else {printf("KA\n");paramAKcom=2;p->M[2]=p->M[2]&0xFFFE;}
+							break;
+							
+							case 1:	
+							//if (p->from_MO3.fromAK.a_params[1]==1) 
+							{paramAKcom=3;p->M[2]=p->M[2]|0x0001;printf("PRD\n");} 
+							//else {printf("KA\n");paramAKcom=4;p->M[2]=p->M[2]&0xFFFE;}break;
 						}
 						break;
+			case 202 :  paramAKcom=5; break;
+			case 264 :  paramAKcom=6; break;
+			case 265 : 	switch(p->from_MO3.fromAK.a_params[0])
+						{
+							case 2:	case 3: paramAKcom=7;break; 
+							case 4: case 5: 
+							case 6:	case 7: 
+							case 8: case 9: paramAKcom=8;break;
+						}
+						break;
+			case 281 : 	switch(p->from_MO3.fromAK.a_params[0])
+						{
+							case 1:	paramAKcom=9;break; 
+							case 2: paramAKcom=10;break;
+						}
+						break;			
+			
+			case 290 :	paramAKcom=11; break;//8410			
+			case 292 : 	switch(p->from_MO3.fromAK.a_params[0])
+						{
+							case 1:	case 2:
+							//case 4:	case 5:
+							case 7:	paramAKcom=15;break; 
+							//case 7:
+							//case 8:	
+							paramAKcom=14;break; 
+							case 3: paramAKcom=13;break;
+							case 9: paramAKcom=12;break;
+						}
+						break;	
 		}
 		switch(paramAKcom)
 		{
-			case 1 : p->M[3]=0x8410;printf("M=1\n");break;
-			case 3 : p->M[3]=0xC430;printf("M=3\n");break;
-			case 2 : p->M[3]=0x9450;printf("M=2\n");break;
-			case 4 : p->M[3]=0xC410;printf("M=4\n");break;
+			case 1  : p->M[3]=0x8400;break;
+			case 2  : p->M[3]=0x8000;break;
+			case 3  : p->M[3]=0xEC30;break;
+			case 4  : p->M[3]=0xE800;break;
+			case 5  : p->M[3]=(p->M[3]&0xF87F)|(p->from_MO3.fromAK.a_params[0]<<7);break;
+			case 6  : case 12 : p->M[3]=0x9450;break;
+			case 7  : case 13 : p->M[3]=0x8C30;break;
+			case 8  : case 11 : case 14 : p->M[3]=0x8410;break;
+			case 9  : p->M[3]=0x8400;break;
+			case 10 : p->M[3]=0x0400;break;
+			case 15 : p->M[3]=0x8210;break;
+			
 		}
+		printf("M=%d\n",paramAKcom);
 	}
 
 	if (p->from_MO3.from41.cr_com!=cr_com41) 
 	{
+		p->from_MO3.from41=rec4.from41;
 		p->num_com=p->from_MO3.from41.num_com;
 		cr_com41=p->from_MO3.from41.cr_com;
 		p->M[0]=0x0000;
 		p->M[1]=0x000e;
-		p->M[2]=0x0000;
-		p->M[3]=0x8410;		
+		p->M[2]=p->from_MO3.from42.M3&0xFFFE;
+		//p->M[3]=0x8410;		
 		printf(" New Command 4.1 = %d\n",p->num_com);
 	}
 	
@@ -332,20 +360,15 @@ while(1)
 			p->to_MO3.toAK.kzv=1;
 			AK_c=0;
 			printf("paramAKcom=%d\n",paramAKcom);
-			switch(paramAKcom)
+			switch(p->to_MO3.toAK.num_com)
 			{
-				case 1 : case 3: case 4: 
-//					if ((p->PR1[3]&0x0004)&&(p->PR1[5]&0x0004)) p->to_MO3.toAK.kzv=0;	break;
-					if (p->PR1[3]&0x0004) p->to_MO3.toAK.kzv=0;	break;
-				case 2 : 		  
-					if ((p->PR1[3]&0x0004)&&(p->PR1[5]&0x0006)) p->to_MO3.toAK.kzv=0;	break;
-//					if (p->PR1[3]&0x0004) p->to_MO3.toAK.kzv=0;	break;
-
+				case 301 : if (p->PR1[3]&0x0004) p->to_MO3.toAK.kzv=0; break;
+				default  : if (p->PR1[5]<7)      p->to_MO3.toAK.kzv=0; break;
 			}
 			//p->M[3]=p->M[3]&0xFFFE; //Отключение ФК СВЧ КА
 			printf("Окончание команды %d. kzv=%d pr1.3=%x pr1.5=%x\n",p->to_MO3.toAK.num_com,p->to_MO3.toAK.kzv,p->PR1[3],p->PR1[5]);
 
-			}
+		}
 		else 	if (AK_c>10)  //2
 				{
 					p->to_MO3.toAK.cr_com=p->from_MO3.fromAK.cr_com;
